3bc7d99f04c83a83110b0ab0a9a9c1d3aae78b02
lawonl4b <lawonl4b@gmail.com>
2026-01-09 09:47:37 +0900

    Solve: [BOJ] 7287 등록

---
+++ ETC/Q7287/Q7287.cpp
@@ -0,0 +1,13 @@
+/*
+[백준 7287번] 등록
+- 난이도: Bronze V
+- 분류: 구현
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "6" << std::endl;
+    std::cout << "piper2026" << std::endl;
+    return 0;
+}

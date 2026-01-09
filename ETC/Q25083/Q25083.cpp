1853b5550484d5f45ba2252db14e411befbb5c48
lawonl4b <lawonl4b@gmail.com>
2026-01-09 10:00:19 +0900

    Solve: [BOJ] 25083 새싹

---
+++ ETC/Q25083/Q25083.cpp
@@ -0,0 +1,17 @@
+/*
+[백준 25083번] 새싹
+- 난이도: Bronze V
+- 분류: 구현
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "         ,r'\"7\n"
+              << "r`-_   ,'  ,/\n"
+              << " \\. \". L_r'\n"
+              << "   `~\\/\n"
+              << "      |\n"
+              << "      |" << std::endl;
+    return 0;
+}

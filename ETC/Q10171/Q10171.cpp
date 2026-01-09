1726d480e89a5c54ee7c68cc6db078d75a6e899e
lawonl4b <lawonl4b@gmail.com>
2026-01-09 09:53:39 +0900

    Solve: [BOJ] 10171 고양이

---
+++ ETC/Q10171/Q10171.cpp
@@ -0,0 +1,15 @@
+/*
+[백준 10171번] 고양이
+- 난이도: Bronze V
+- 분류: 구현
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "\\    /\\\n"
+              << " )  ( ')\n"
+              << "(  /  )\n"
+              << " \\(__)|" << std::endl;
+    return 0;
+}

8091ad405e59269f9719455db1a7cda3f3069b05
lawonl4b <lawonl4b@gmail.com>
2026-01-15 01:12:26 +0900

    Solve: [BOJ] 10872 팩토리얼

---
+++ Mathematics/Q10872/Q10872.cpp
@@ -0,0 +1,21 @@
+/*
+[백준 110872번] 팩토리얼
+- 난이도: Bronze V
+- 분류: 구현, 수학
+*/
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    int result = 1;
+
+    std::cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        result *= i;
+    }
+    std::cout << result;
+
+    return 0;
+}

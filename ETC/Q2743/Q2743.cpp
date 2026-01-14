035032863d75eed739a52401ac0cb0e98bed383a
lawonl4b <lawonl4b@gmail.com>
2026-01-15 03:08:53 +0900

    Solve: [BOJ] 2743 단어 길이 재기

---
+++ ETC/Q2743/Q2743.cpp
@@ -0,0 +1,22 @@
+/*
+[백준 2743번] 단어 길이 재기
+- 난이도: Bronze Ⅴ
+- 분류: 구현, 문자열
+- 언어: C++(17)
+- 메모리: 2024 KB
+- 시간: 0 ms
+*/
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::string str;
+    std::cin >> str;
+    std::cout << str.length() << "\n";
+
+    return 0;
+}
